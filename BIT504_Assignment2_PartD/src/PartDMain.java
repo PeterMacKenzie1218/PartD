78cb16bc881ef15a3c9ef08d60fd5abe20e6c557
PeterMacKenzie1218 <petermackenzie@ymail.com>
2018-09-16 21:49:03 +1200

    Second Commit

--- BIT504_Assignment2_PartD/src/PartDMain.java
+++ BIT504_Assignment2_PartD/src/PartDMain.java
@@ -4,6 +4,8 @@ public class PartDMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi, my name is Peter.");
+		
+		System.out.println("My favourite colour is Blue.");
 
 	}
 
